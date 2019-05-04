6704a9f837ae04d3366b984660fcc4274170c0c8
andrewawni <andrewawnii@gmail.com>
2019-05-04 22:34:02 +0200

    Changes to be committed: 	deleted:    bin/main 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
+#include <array>
+#include <bits/stdc++.h>
 #include <cstdio>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
 #include <string>
-#include <array>
-#include <bits/stdc++.h>
 
 using namespace std;
 
-
 void classifyRecursively(string);
-std::string exec(const char* cmd) {
 
-    std::array<char,128> buffer;
+std::string exec(const char* cmd)
+{
+
+    std::array<char, 128> buffer;
     std::string result;
     /* 
     unique_ptr is a smart pointer that has two components:
@@ -20,8 +21,8 @@ std::string exec(const char* cmd) {
         2-a stored deleter: a callable object that takes an argument of the same type as the stored pointer and is called to delete the managed object
     */
 
-    std::unique_ptr<FILE, decltype(&pclose)> pipe (popen(cmd, "r"), pclose);
-    
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+
     if (!pipe) {
         throw std::runtime_error("popen() failed!");
     }
@@ -43,12 +44,11 @@ int main(int argc, char** argv)
 
     // }
 
-  string option = argv[1];
-  if(option == "-r")
-  {
-      classifyRecursively(argv[2]);
-      cout << "here";
-  }
+    string option = argv[1];
+    if (option == "-r") {
+        classifyRecursively(argv[2]);
+        cout << "here";
+    }
     return 0;
 }
 
@@ -56,22 +56,29 @@ int main(int argc, char** argv)
 /home/undo/Development/Projects/FileClassifier/testingDir
 */
 
-
 class Node {
-        public:
-        int cntNodes = 0;
-        string selfPath;
-        string parentPath;
-        Node* children[];
+public:
+    int cntNodes = 0;
+    string selfPath;
+    string parentPath;
+    Node* children[];
 };
-string TypeOfFile(string s){
-    if(s=="mp3")return "music";
-    else if(s=="mp4")return "video";
-    else if(s=="png" || s=="jpg")return "image";
-    else if(s=="txt")return "document";
-    else return "not known";
+
+string TypeOfFile(string s)
+{
+    if (s == "mp3")
+        return "music";
+    else if (s == "mp4")
+        return "video";
+    else if (s == "png" || s == "jpg")
+        return "image";
+    else if (s == "txt")
+        return "document";
+    else
+        return "not known";
 }
-vector<pair<string, string> > getContent(string _rootDir) {
+vector<pair<string, string>> getContent(string _rootDir)
+{
 
     vector<pair<string, string>> ret;
     string cmd = "";
@@ -79,33 +86,30 @@ vector<pair<string, string> > getContent(string _rootDir) {
     cmd += _rootDir;
     cmd += " -p ";
     cmd += "| grep -v /";
-    char* temp = new char[cmd.size()+1];
-    strcpy(temp, cmd.c_str()); 
+    char* temp = new char[cmd.size() + 1];
+    strcpy(temp, cmd.c_str());
     string content = exec(temp);
 
     string tempContent = "";
-    for(int i = 0; i<content.size(); i++)
-    {
-        if(content[i] != '\n')
-            tempContent+=content[i];
-        else
-        {
-            string type="";
-            for(int j=0; j<tempContent.size(); j++){
-                if(tempContent[j]=='.'){
-                    for(int k=j+1; k<tempContent.size(); k++){
-                        type+=tempContent[k];
-                }
-                
-                }
-                else{
+    for (int i = 0; i < content.size(); i++) {
+        if (content[i] != '\n')
+            tempContent += content[i];
+        else {
+            string type = "";
+            for (int j = 0; j < tempContent.size(); j++) {
+                if (tempContent[j] == '.') {
+                    for (int k = j + 1; k < tempContent.size(); k++) {
+                        type += tempContent[k];
+                    }
+
+                } else {
                     continue;
                 }
-                }
-            
-            ret.push_back({_rootDir+tempContent, TypeOfFile(type)});
+            }
+
+            ret.push_back({ _rootDir + tempContent, TypeOfFile(type) });
             tempContent = "";
-            i+=1;
+            i += 1;
         }
     }
     return ret;
@@ -119,46 +123,43 @@ void classifyRecursively(string _rootDir)
     root->selfPath = rootDir;
     root->parentPath = " ";
 
-    
     string cmd = "";
     cmd += "ls ";
     cmd += rootDir;
     cmd += " -p ";
     cmd += "| grep /";
 
+    char* temp = new char[cmd.size() + 1];
+    strcpy(temp, cmd.c_str());
 
-    char* temp = new char[cmd.size()+1];
-    strcpy(temp, cmd.c_str()); 
-
-    string children = exec(temp); 
+    string children = exec(temp);
     int lastIndex = 0;
     //cout<<children.size();
 
-    string tmp="";
-    for(int i=0; i<children.size(); i++){
-        if(children[i]!='/'){
-            tmp+=children[i];
-        }
-        else{
-              Node* child=new Node();
-              child->parentPath=rootDir;
-              child->selfPath = child->parentPath + "/" + tmp;
-              root->children[root->cntNodes++] = child;
-              tmp="";
-              i+=1;
+    string tmp = "";
+    for (int i = 0; i < children.size(); i++) {
+        if (children[i] != '/') {
+            tmp += children[i];
+        } else {
+            Node* child = new Node();
+            child->parentPath = rootDir;
+            child->selfPath = child->parentPath + "/" + tmp;
+            root->children[root->cntNodes++] = child;
+            tmp = "";
+            i += 1;
         }
     }
-    for(int i = 0; i<root->cntNodes; i++)
-    {
+    for (int i = 0; i < root->cntNodes; i++) {
         // cout << root->children[i]->selfPath << endl;
-        vector<pair <string,string> > tmp=getContent(root->children[i]->selfPath);
-        for(int j=0; j<tmp.size(); j++){
-            cout<<tmp[j].first<<" "<<tmp[j].second<<endl;
+        vector<pair<string, string>> tmp = getContent(root->children[i]->selfPath);
+        for (int j = 0; j < tmp.size(); j++) {
+            cout << tmp[j].first << " " << tmp[j].second << endl;
         }
         classifyRecursively(root->children[i]->selfPath);
-        
-        
     }
 
-    
+    for (int i = 0; i < 5; i++) {
+        vector<int> v;
+        v.push_back(5);
     }
+}
